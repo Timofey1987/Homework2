fa93521928c18806b6eec539afbf372acbdbe8b3
Timofey1987 <son_decline@mail.ru>
2024-06-27 10:53:02 +0800

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+completing_homework = 12
+execution_time = 1.5
+name_of_the_course = 'Python'
+time_to_complete_one_task = (execution_time / completing_homework)
+print('Курс:', name_of_the_course + ',', 'всего задач:', str(completing_homework) + ',', 'затрачено часов:', str(execution_time) + ',', 'среднее время выполнения', time_to_complete_one_task, 'часа.')
